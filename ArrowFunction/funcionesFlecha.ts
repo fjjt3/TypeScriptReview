4e09b3add0077a040435d01a65faef0ccfe6ff05
Francisco Javier Jimenez <>
2024-10-21 11:04:01 +0200

    Add: Arrow and decorators

---
+++ ArrowFunction/funcionesFlecha.ts
@@ -0,0 +1,13 @@
+let suma = function (a:number, b: number){
+    return a + b;
+};
+
+console.log(suma(5 ,3));
+
+let sumaFlecha = (a:number, b: number) =>  a + b;
+
+console.log(sumaFlecha(5 ,4));
+
+let getNameArrow = () => "Luke Skywalker";
+
+console.log(getNameArrow());
